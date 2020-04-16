7aee410b74b35257fc420448ae8cd620a19a2ad6
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-16 06:51:09 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,3 +8,10 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 
+class CreateCostumes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+    end
+  end
+end
