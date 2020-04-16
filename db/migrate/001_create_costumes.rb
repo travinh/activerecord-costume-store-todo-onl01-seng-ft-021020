fb80d32b3a1af599355fb7f06675b38c5e59a14d
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-16 06:52:31 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,8 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t..
+      t.string :size 
+      t.
     end
   end
 end
