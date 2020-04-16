059763d0d44fbab2e041b1d4baeedc1b1353a2d2
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-16 06:55:17 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,13 +3,13 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costume_stores do |t|
       t.string :name 
-      s.string :location 
-      s.integer :costume_inventory 
-      s.integer :num_of_employees
-      s.string :business_status 
-      s.boolean :still_in_business
-      s.datetime :opening_time
-      s.datetime :closing_time 
+      t.string :location 
+      t.integer :costume_inventory 
+      t.integer :num_of_employees
+      t.string :business_status 
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time 
     end
   end
 end
