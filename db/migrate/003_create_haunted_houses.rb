198a4d34ed7369299ec239b1a7b9bf1764272413
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-16 06:54:51 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |t|
+      s.string :name 
+      s.string :location 
+      s.integer :costume_inventory 
+      s.integer :num_of_employees
+      s.string :business_status 
+      s.boolean :still_in_business
+      s.datetime :opening_time
+      s.datetime :closing_time 
+    end
+  end
+end
