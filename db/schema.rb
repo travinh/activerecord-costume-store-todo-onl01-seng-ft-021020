741c693a6b6aa6c3a7ff11a80557c6225c7854cd
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-16 07:00:40 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,17 @@ ActiveRecord::Schema.define(version: 2) do
     t.float "price"
     t.string "size"
     t.string "image_url"
+  end
+
+  create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.float "price"
+    t.boolean "family_friendly"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.text "description"
   end
 
 end
