340df67cf4bff1fd556f48716b4ec84e504f6881
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-16 06:53:57 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,7 +1,7 @@
 # Create your costume_stores migration here
-class CreateCostumes < ActiveRecord::Migration[5.1]
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
-    create_table :costumes do |t|
+    create_table :costume_stores do |t|
       t.string :name
       t.float :price
       t.string :size 
